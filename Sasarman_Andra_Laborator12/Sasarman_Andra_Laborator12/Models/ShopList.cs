c0615b0e1ab8c72ba9d603cdbee4238e5b84861c
ANDRA-FLORINA-MARIA SĂSĂRMAN <andra.sasarman@stud.ubbcluj.ro>
2021-01-07 16:37:01 +0200

    Add project files.

---
+++ Sasarman_Andra_Laborator12/Sasarman_Andra_Laborator12/Models/ShopList.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sasarman_Andra_Laborator12.Models
+{
+    public class ShopList
+    {
+        public int ID { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
